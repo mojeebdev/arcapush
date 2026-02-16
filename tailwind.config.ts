1dbb9f41789a9ad86ceb94c0b6d0201e39c28cf4
oxmojeeb <mojeeb.eth@gmail.com>
2026-02-16 21:38:40 +0100

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  "./src/**/*.{js,ts,jsx,tsx,mdx}", 
+],
   darkMode: "class",
   theme: {
     extend: {
