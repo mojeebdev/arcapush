c29c44a53657a5d46599b3f6f70f7fa301c6ed68
Mojeeb Titilayo <142301831+mojeebdev@users.noreply.github.com>
2026-03-20 19:25:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,32 @@
-/** @type {import(‘next’).NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-generateBuildId: async () => `build-${Date.now()}`,
+  generateBuildId: async () => {
+    return 'build-' + Date.now();
+  },
 
-images: {
-remotePatterns: [{ protocol: “https”, hostname: “**” }],
-},
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
 
-transpilePackages: [
-“@solana/wallet-adapter-base”,
-“@solana/wallet-adapter-react”,
-“@solana/wallet-adapter-react-ui”,
-“@solana/wallet-adapter-wallets”,
-“@solana/web3.js”,
-],
+  transpilePackages: [
+    '@solana/wallet-adapter-base',
+    '@solana/wallet-adapter-react',
+    '@solana/wallet-adapter-react-ui',
+    '@solana/wallet-adapter-wallets',
+    '@solana/web3.js',
+  ],
 
-serverExternalPackages: [”@prisma/client”, “pg”],
+  serverExternalPackages: ['@prisma/client', 'pg'],
 
-outputFileTracingIncludes: {
-“/blog/[slug]”: [”./content/blog/**/*”],
-},
+  outputFileTracingIncludes: {
+    '/blog/[slug]': ['./content/blog/**/*'],
+  },
 
-experimental: {
-serverActions: {
-allowedOrigins: [“arcapush.com”, “www.arcapush.com”],
-},
-},
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['arcapush.com', 'www.arcapush.com'],
+    },
+  },
 };
 
 module.exports = nextConfig;
